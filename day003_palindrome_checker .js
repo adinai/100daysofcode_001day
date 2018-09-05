fcf686a9d866f66d7e23a4f9232cfa889b260507
Adina Kaiymova <adina.kaiymova@gmail.com>
2018-09-05 23:57:08 +0900

    Update day003_palindrome_checker .js

--- day003_palindrome_checker .js
+++ day003_palindrome_checker .js
@@ -3,6 +3,37 @@
 
 // A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
+// solution 1
+
+function palindrome(str) {
+  // Good luck!
+  return true;
+  var reg = /[\W_]/g;
+  var lower_string = str.toLowerCase().replace(reg, "" );
+  var reversed = lower_string.split(" ").reverse().join(" ");
+  if(reversed === lower_string)
+    return true; 
+    
+    return false;
+   
+}
+
+
+
+palindrome("eye");
+
+
+
+
+
+//=========================///
+  function palindrome(str) {
+      return str.replace(/[\W_]/g, '').toLowerCase() ===
+             str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+    }
+//======================//
+
+
 function palindrome(str) {
   str = str.toLowerCase().replace(/[\W_]/g, ''); //regexone 
   for(var i = 0, len = str.length - 1; i < len/2; i++) {
@@ -12,3 +43,4 @@ function palindrome(str) {
   }
   return true;
 }
+
